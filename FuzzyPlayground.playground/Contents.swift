4cb96fefcebabca94bd7a90bf021d0ca908ce11e
Jose Maria <josemaria.puerta@bq.com>
2017-01-20 23:06:06 +0100

    Playground performance time

--- FuzzyPlayground.playground/Contents.swift
+++ FuzzyPlayground.playground/Contents.swift
@@ -2,11 +2,14 @@
 import FuzzySwift
 import XCPlayground
 
+let start = CACurrentMediaTime()
+
 let ruleSet: SpeedRuleSet = SpeedRuleSet()
 
 let system = InferenceManager(ruleSets: [ruleSet])
 system.set(input: 10, for: ruleSet.frontalDistance)
 let evaluated = system.evaluate(variable: ruleSet.speed)
 
+let end = CACurrentMediaTime()
 
-print("Hola")
+print("Ext time: \(end - start)")
